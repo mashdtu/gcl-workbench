cbc50c5190f34f3ae64e87d64aef6518b3a18300
mash <mashdtu@pm.me>
2026-05-01 19:06:23 +0200

    removed fsharp dir again oopsies

--- src/fsharp/SignAnalysis.fs
+++
@@ -1,5 +0,0 @@
-module SignAnalysis
-open Io.SignAnalysis
-
-let analysis (input: Input) : Output =
-    failwith "Sign analysis not yet implemented" // TODO: start here
--- src/fsharp/Types.fs
+++
@@ -1,3 +0,0 @@
-module Types
-
-// Use this file to define any types you need across multiple analysis modules
